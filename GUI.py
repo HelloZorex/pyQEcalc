c34a5d5e1d650dd477bb6cea30e91e5f440485a3
Grant Cary <85152448+HelloZorex@users.noreply.github.com>
2021-10-02 23:09:22 -0700

    new gui file

---
+++ GUI.py
@@ -0,0 +1,138 @@
+import sys
+import tkinter as tk
+import QuadraticCalc as qc
+
+sys.path.append(".")
+
+root = tk.Tk()
+
+def clear():
+    buttons = root.grid_slaves()
+    for cycle in buttons:
+        cycle.destroy()
+
+def exe1():
+    aV = e01.get()
+    bV = e02.get()
+    cV = e03.get()
+    aV, bV, cV = int(aV), int(bV), int(cV)
+    qc.standardsolution(aV, bV, cV)
+    xCoord = qc.xCoord
+    yCoord = qc.xCoord
+    strVar = qc.strVar
+    ans1 = tk.Label(root, text=f"answer: x-intercept {xCoord}, y-intercept {yCoord}").grid(row=5, column=1)
+    ans2 = tk.Label(root, text=f"{strVar}").grid(row=6, column=1)
+
+def exe2():
+    aV = e11.get()
+    bV = e12.get()
+    cV = e13.get()
+    aV, bV, cV = int(aV), int(bV), int(cV)
+    qc.vertex(aV, bV, cV)
+    xCoord = qc.xCoord
+    yCoord = qc.xCoord
+    strVar = qc.strVar
+    ans1 = tk.Label(root, text=f"answer: x-intercept {xCoord}, y-intercept {yCoord}").grid(row=5, column=1)
+    ans2 = tk.Label(root, text=f"{strVar}").grid(row=6, column=1)
+
+def exe3():
+    aV = e21.get()
+    mV = e22.get()
+    bV = e23.get()
+    nV = e24.get()
+    cV = e25.get()
+    aV, mV, bV, nV, cV = int(aV), int(mV), int(bV), int(nV), int(cV)
+    qc.zeros(aV, mV, bV, nV, cV)
+    xCoord = qc.xCoord
+    yCoord = qc.xCoord
+    strVar = qc.strVar
+    ans1 = tk.Label(root, text=f"answer: x-intercept {xCoord}, y-intercept {yCoord}").grid(row=7, column=1)
+    ans2 = tk.Label(root, text=f"{strVar}").grid(row=8, column=1)
+
+def opt1():
+    tk.Label(root, text="ax^2 + bx + c").grid(row=0, column=1)
+    global e01
+    global e02
+    global e03
+    
+    e01 = tk.Entry(root)
+    e02 = tk.Entry(root)
+    e03 = tk.Entry(root)
+
+    e01.grid(row=1, column=1)
+    e02.grid(row=2, column=1)
+    e03.grid(row=3, column=1)
+
+    tk.Label(root, text="a: ").grid(row=1, column=0)
+    tk.Label(root, text="b: ").grid(row=2, column=0)
+    tk.Label(root, text="c: ").grid(row=3, column=0)
+
+    tk.Button(root, text="Quit", command=quit).grid(row=4, column=0)
+    tk.Button(root, text="Calculate", command=exe1).grid(row=4, column=1)
+
+def opt2():
+    tk.Label(root, text="a(x-h)^2 + k").grid(row=0, column=1)
+    
+    global e11
+    global e12
+    global e13
+    
+    e11 = tk.Entry(root)
+    e12 = tk.Entry(root)
+    e13 = tk.Entry(root)
+
+    e11.grid(row=1, column=1)
+    e12.grid(row=2, column=1)
+    e13.grid(row=3, column=1)
+
+    tk.Label(root, text="a: ").grid(row=1, column=0)
+    tk.Label(root, text="h: ").grid(row=2, column=0)
+    tk.Label(root, text="k: ").grid(row=3, column=0)
+
+    tk.Button(root, text="Quit", command=quit).grid(row=4, column=0)
+    tk.Button(root, text="Calculate", command=exe2).grid(row=4, column=1)
+
+def opt3():
+    tk.Label(root, text="a(mx+b)(nx+c)").grid(row=0, column=1)
+    
+    global e21
+    global e22
+    global e23
+    global e24
+    global e25
+
+    e21 = tk.Entry(root)
+    e22 = tk.Entry(root)
+    e23 = tk.Entry(root)
+    e24 = tk.Entry(root)
+    e25 = tk.Entry(root)
+
+    e21.grid(row=1, column=1)
+    e22.grid(row=2, column=1)
+    e23.grid(row=3, column=1)
+    e24.grid(row=4, column=1)
+    e25.grid(row=5, column=1)
+
+    tk.Label(root, text="a: ").grid(row=1, column=0)
+    tk.Label(root, text="m: ").grid(row=2, column=0)
+    tk.Label(root, text="b: ").grid(row=3, column=0)
+    tk.Label(root, text="n: ").grid(row=4, column=0)
+    tk.Label(root, text="c: ").grid(row=5, column=0)
+
+    tk.Button(root, text="Quit", command=quit).grid(row=6, column=0)
+    tk.Button(root, text="Calculate", command=exe3).grid(row=6, column=1)
+
+def selector():
+    button1 = tk.Button(root, text="Standard", command=lambda:[clear(), opt1()])
+    button1.grid(row=1, column=1)
+    button2 = tk.Button(root, text="Vertex", command=lambda:[clear(), opt2()])
+    button2.grid(row=2, column=1)
+    button3 = tk.Button(root, text="0's", command=lambda:[clear(), opt3()])
+    button3.grid(row=3, column=1)
+    button4 = tk.Button(root, text="Quit", command=quit)
+    button4.grid(row=4, column=1)
+
+selector()
+
+root.mainloop()
+ 
--- QuadraticCalc.py
+++ QuadraticCalc.py
@@ -30,7 +30,7 @@ def standardsolution(num1, num2, num3):
     global strVar
     xCoord = float(round((-num2/(2*num1)), 2))
     yCoord = float(round(((num1*xCoord**2)+(num2*xCoord)+num3), 2))
-    print(f" vertex = x: {xCoord}, y: {yCoord}")
+    #print(f" vertex = x: {xCoord}, y: {yCoord}")
     disc = num2**2-4*num1*num3
     if disc < 0:
         #imaginary number
@@ -70,25 +70,32 @@ def standardsolution(num1, num2, num3):
                         if c02 == 1 or c02 == -1:
                             #if number next to square root is 1 or -1 and denominator is 1 or -1, don't print it
                             strVar = (f" 0's = {c01} + sqrt({sq02}) and {c01} - sqrt({sq02})")
+                            return strVar
                         else:
                             #if denominator is 1 or -1, don't print it
                             strVar = (f" 0's = {c01} + {c02} sqrt({sq02}) and {c01} - {c02} sqrt({sq02})")
+                            return strVar
                     else:
                         #if denominator is not 1 or -1, print it
                         strVar = (f" 0's = ({c01} + {c02} sqrt({sq02})) / {d02} and ({c01} - {c02} sqrt({sq02})) / {d02}")
+                        return strVar
                 else:
                     #different denominators
                     if c01 == d02 or c01 == -d02:
                         if c02 == 1 or c02 == -1:                    
                             strVar = (f" 0's = ({c01} + sqrt({sq02})) / {d03} and ({c01} - sqrt({sq02})) / {d03}")
+                            return strVar
                         else:
                             strVar = (f" 0's = ({c01} + {c02} sqrt({sq02})) / {d03} and ({c01} - {c02} sqrt({sq02})) / {d03}")
+                            return strVar
                     else:
                         if c02 == 1 or c02 == -1:                    
                             strVar = (f" 0's = ({c01} / {d02} + sqrt({sq02})) / {d03} and ({c01} / {d02} - sqrt({sq02})) / {d03}")
+                            return strVar
                         else:
                             strVar = (f" 0's = ({c01} / {d02} + {c02} sqrt({sq02})) / {d03} and ({c01} / {d02} - {c02} sqrt({sq02})) / {d03}")
-            
+                            return strVar
+
             if not square_list:
                 null_01 = 0
                 while null_01 < disc:
@@ -104,17 +111,20 @@ def standardsolution(num1, num2, num3):
                 reducefract(-num2, d01, fraction_list)
                 join_tree = ", ".join(map(str, factor_tree))
                 strVar = (f" ({fraction_list[0]} +- sqrt ({join_tree})) / {fraction_list[1]}")
+                return strVar
 
         else:
             #is prime number
             strVar = (f" (-{num2} +- sqrt ({disc})) / {2*num1}")
-        
+            return strVar
+
     elif sqrt(disc).is_integer() == True:
         #perfect square
         add = (-num2 + (disc)**0.5)/(2*num1)
         subtract = (-num2 - (disc)**0.5)/(2*num1)
         strVar = (f" 0's = {add, subtract}")
-    
+        return strVar
+
     else:
         print("undefined")
         error_list = [num1, num2, num3, "\n"]
@@ -160,129 +170,6 @@ def zeros(num1, mvar, num2, nvar, num3):
     standardsolution(binom1, binom2, binom3)
 
 #selection()
-
-root = tk.Tk()
-
-def clear():
-    buttons = root.grid_slaves()
-    for cycle in buttons:
-        cycle.destroy()
-
-def exe1():
-    aV = e01.get()
-    bV = e02.get()
-    cV = e03.get()
-    aV, bV, cV = int(aV), int(bV), int(cV)
-    standardsolution(aV, bV, cV)
-    ans1 = tk.Label(root, text=f"answer: x-intercept {xCoord}, y-intercept {yCoord}").grid(row=5, column=1)
-    ans2 = tk.Label(root, text=f"{strVar}").grid(row=6, column=1)
-
-def exe2():
-    aV = e11.get()
-    bV = e12.get()
-    cV = e13.get()
-    aV, bV, cV = int(aV), int(bV), int(cV)
-    vertex(aV, bV, cV)
-    ans1 = tk.Label(root, text=f"answer: x-intercept {xCoord}, y-intercept {yCoord}").grid(row=5, column=1)
-    ans2 = tk.Label(root, text=f"{strVar}").grid(row=6, column=1)
-
-def exe3():
-    aV = e21.get()
-    mV = e22.get()
-    bV = e23.get()
-    nV = e24.get()
-    cV = e25.get()
-    aV, mV, bV, nV, cV = int(aV), int(mV), int(bV), int(nV), int(cV)
-    zeros(aV, mV, bV, nV, cV)
-    ans1 = tk.Label(root, text=f"answer: x-intercept {xCoord}, y-intercept {yCoord}").grid(row=7, column=1)
-    ans2 = tk.Label(root, text=f"{strVar}").grid(row=8, column=1)
-
-def opt1():
-    tk.Label(root, text="ax^2 + bx + c").grid(row=0, column=1)
-    global e01
-    global e02
-    global e03
-    
-    e01 = tk.Entry(root)
-    e02 = tk.Entry(root)
-    e03 = tk.Entry(root)
-
-    e01.grid(row=1, column=1)
-    e02.grid(row=2, column=1)
-    e03.grid(row=3, column=1)
-
-    tk.Label(root, text="a: ").grid(row=1, column=0)
-    tk.Label(root, text="b: ").grid(row=2, column=0)
-    tk.Label(root, text="c: ").grid(row=3, column=0)
-
-    tk.Button(root, text="Quit", command=quit).grid(row=4, column=0)
-    tk.Button(root, text="Calculate", command=exe1).grid(row=4, column=1)
-
-def opt2():
-    tk.Label(root, text="a(x-h)^2 + k").grid(row=0, column=1)
-    
-    global e11
-    global e12
-    global e13
-    
-    e11 = tk.Entry(root)
-    e12 = tk.Entry(root)
-    e13 = tk.Entry(root)
-
-    e11.grid(row=1, column=1)
-    e12.grid(row=2, column=1)
-    e13.grid(row=3, column=1)
-
-    tk.Label(root, text="a: ").grid(row=1, column=0)
-    tk.Label(root, text="h: ").grid(row=2, column=0)
-    tk.Label(root, text="k: ").grid(row=3, column=0)
-
-    tk.Button(root, text="Quit", command=quit).grid(row=4, column=0)
-    tk.Button(root, text="Calculate", command=exe2).grid(row=4, column=1)
-
-def opt3():
-    tk.Label(root, text="a(mx+b)(nx+c)").grid(row=0, column=1)
-    
-    global e21
-    global e22
-    global e23
-    global e24
-    global e25
-
-    e21 = tk.Entry(root)
-    e22 = tk.Entry(root)
-    e23 = tk.Entry(root)
-    e24 = tk.Entry(root)
-    e25 = tk.Entry(root)
-
-    e21.grid(row=1, column=1)
-    e22.grid(row=2, column=1)
-    e23.grid(row=3, column=1)
-    e24.grid(row=4, column=1)
-    e25.grid(row=5, column=1)
-
-    tk.Label(root, text="a: ").grid(row=1, column=0)
-    tk.Label(root, text="m: ").grid(row=2, column=0)
-    tk.Label(root, text="b: ").grid(row=3, column=0)
-    tk.Label(root, text="n: ").grid(row=4, column=0)
-    tk.Label(root, text="c: ").grid(row=5, column=0)
-
-    tk.Button(root, text="Quit", command=quit).grid(row=6, column=0)
-    tk.Button(root, text="Calculate", command=exe3).grid(row=6, column=1)
-
-def selector():
-    button1 = tk.Button(root, text="Standard", command=lambda:[clear(), opt1()])
-    button1.grid(row=1, column=1)
-    button2 = tk.Button(root, text="Vertex", command=lambda:[clear(), opt2()])
-    button2.grid(row=2, column=1)
-    button3 = tk.Button(root, text="0's", command=lambda:[clear(), opt3()])
-    button3.grid(row=3, column=1)
-    button4 = tk.Button(root, text="Quit", command=quit)
-    button4.grid(row=4, column=1)
-
-selector()
-
-root.mainloop()
 
 #for t1 in range(1, 10):
 #    for t2 in range(1, 10):
